b5b7b54265958fa2b0b4022eadd07f4c86eec703
Bruno <bfhenriques178@gmail.com>
2018-03-21 16:58:49 +0000

    betting centre update

--- Races/src/Monitores/BettingCentre.java
+++ Races/src/Monitores/BettingCentre.java
@@ -5,13 +5,21 @@ import Threads.Broker;
 import Interfaces.IBettingCentre_Broker;
 import Interfaces.IBettingCentre_Spectator;
 import Enum.*;
+import Main.*;
+import java.util.Collection;
+import java.util.HashMap;
 
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.locks.*;
 
 public class BettingCentre implements IBettingCentre_Spectator, IBettingCentre_Broker{
     private Queue<Spectator> fifoSpectators = new LinkedList<Spectator>();
+    
+    private Map<Integer, Integer> mapSpec_Horse = new HashMap<Integer, Integer>();
+    private Map<Integer,Double> mapSpec_Bet = new HashMap<Integer, Double>();
     
     private GRI gri;
     private final ReentrantLock rl;
@@ -19,9 +27,17 @@ public class BettingCentre implements IBettingCentre_Spectator, IBettingCentre_B
     private final Condition condBroker;
     private final Condition condSpectators;
     private int nSpectators = 0;
+    private int NO_SPECTATORS = Main.NO_SPECTATORS;
+    private boolean betDone = false;
+    private boolean wantToBet = false;
+    private boolean betSoec [];
     
     public BettingCentre(GRI gri){
         this.gri = gri;
+        betSoec = new boolean[NO_SPECTATORS];
+        for(int i=0; i< NO_SPECTATORS; i++){
+                betSoec[i] = false;
+        }
         rl = new ReentrantLock();
         condHorses = rl.newCondition();
         condBroker = rl.newCondition();
@@ -41,6 +57,16 @@ public class BettingCentre implements IBettingCentre_Spectator, IBettingCentre_B
                 spec.setSpecState(SpectatorState.PLACING_A_BET);
                 System.out.println("Spectator " + spec.getSpecId() + " " + spec.getSpecState());
                 
+                wantToBet = true;
+                condBroker.signal();
+                
+                while (betSoec[spec.getSpecId()] == true){
+                    condSpectators.await();
+                }
+                System.out.println("sai do while " + spec.getSpecId());
+                //condBroker.await();
+                //condSpectators.signal();
+                
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -57,10 +83,26 @@ public class BettingCentre implements IBettingCentre_Spectator, IBettingCentre_B
         Broker broker = ((Broker)Thread.currentThread());
         System.out.println("acceptBets");
         
+        int bets = 0;
+        
         try{
             try{
                 broker.setBroState(BrokerState.WAITING_FOR_BETS);
                 System.out.println("Broker " + broker.getBroState());
+                System.out.println("bets 1 -- "+bets);
+                while (bets != NO_SPECTATORS){
+                    System.out.println("bets 2 -- "+bets);
+                    if(wantToBet){
+                        System.out.println("WantToBet - " + fifoSpectators.peek().getSpecId());
+                        bets++;
+                        int id = fifoSpectators.peek().getSpecId();
+                        betSoec[id] = true;
+                        mapSpec_Horse.put(id, 0);
+                        mapSpec_Bet.put(fifoSpectators.remove().getSpecId(), 5.0);
+                        condSpectators.signalAll();
+                    }
+                    condBroker.await();
+                } 
                 
                 
                 
